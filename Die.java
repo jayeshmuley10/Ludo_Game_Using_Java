808deab0d4faa581e8820da87c85f5535025f2aa
Jayesh Muley <115483595+jayeshmuley10@users.noreply.github.com>
2022-11-12 23:13:30 +0530

    Delete Die.java

--- Die.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-public class Die {
-    public static void main(String args[]) {
-        Random random = new Random();
-        System.out.print("The value on dice is: "
-                + (random.nextInt(6) + 1));
-    }
-}
